7f2dd0a99a13bcf5373130b78dbc6ca404562217
RHEZUS <ludivinseunda2@gmail.com>
2023-10-14 13:07:54 -0700

    Project formating

--- main.h
+++ main.h
@@ -2,7 +2,21 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
 
 
 #endif
