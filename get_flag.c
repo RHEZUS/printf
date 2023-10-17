bed6a5da71e58ac08e0f1c677e31c5a245deffa1
RHEZUS <ludivinseunda2@gmail.com>
2023-10-17 11:07:36 -0700

    all problems solved

--- get_flag.c
+++ get_flag.c
@@ -8,7 +8,6 @@
  */
 int get_flags(const char *format, int *i)
 {
-<<<<<<< HEAD
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
 	int j, curr_i;
@@ -28,33 +27,8 @@ int get_flags(const char *format, int *i)
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
-=======
-        /* - + 0 # ' ' */
-        /* 1 2 4 8  16 */
-        int j, curr_i;
-        int flags = 0;
-        const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-        const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-        for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-        {
-                for (j = 0; FLAGS_CH[j] != '\0'; j++)
-                        if (format[curr_i] == FLAGS_CH[j])
-                        {
-                                flags |= FLAGS_ARR[j];
-                                break;
-                        }
+	*i = curr_i - 1;
 
-                if (FLAGS_CH[j] == 0)
-                        break;
-        }
->>>>>>> wolf
-
-        *i = curr_i - 1;
-
-<<<<<<< HEAD
 	return (flags);
-=======
-        return (flags);
->>>>>>> wolf
 }
--- get_precision.c
+++ get_precision.c
@@ -38,3 +38,4 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+
