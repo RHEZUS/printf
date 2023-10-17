2aa36cfe1012dab22fde2dc98737d8249dd1475a
RHEZUS <ludivinseunda2@gmail.com>
2023-10-17 09:17:36 -0700

    last work

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
@@ -9,39 +10,45 @@
 
 int _printf(const char *format, ...)
 {
-	int i, printed_chars = 0, width = 0, size = 0;
-	int precision = 0, flags = 0, printed = 0;
-	va_list args;
+	int i = 0, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
-	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			/*write(1, &format[i], 1);*/
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
-			write(1, &format[i], 1);
 		}
 		else
 		{
-			flags = get_flag(format, &i);
-			width = get_width(format, &i);
-			precision = get_precision(format, &i, args);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-
-			i++;
-
-			printed = print_handler(format, &i, args, flags, width, precision, size);
-
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
-
 	}
-	va_end(args);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
 	return (printed_chars);
 }
--- get_flag.c
+++ get_flag.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
- * get_flag - Calculating all active flag
- * @format: string in which to print arr
- * @i: taking a parameter
- * Return: Flag
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
  */
-int get_flag(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
 	int j, curr_i;
-	int flag = 0;
-	const char flag_ch[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int flag_arr[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; flag_ch[j] != '\0'; j++)
-			if (format[curr_i] == flag_ch[j])
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flag = flag_arr[j];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
-		if (flag_ch[j] == 0)
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
 	*i = curr_i - 1;
 
-	return (flag);
+	return (flags);
 }
-
+/* Collaborators: Juskins and MCCHANCE  */
